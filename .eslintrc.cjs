1907deca534cde071dcc1d7fac6c6e82053af709
anandu.reghu <anandu.reghu@qburst.com>
2022-12-10 22:49:35 +0530

    added translation

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -27,6 +27,7 @@ module.exports = {
   rules: {
     'react/react-in-jsx-scope': 0,
     'react/function-component-definition': 0,
+    'import/prefer-default-export': 0,
     'no-restricted-imports': 'off',
     '@typescript-eslint/no-restricted-imports': [
       'warn',
